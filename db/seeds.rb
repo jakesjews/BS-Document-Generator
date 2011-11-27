a851f7b8d1b4ff8ce07bdd762b58d3c825969a4b
Jacob Jewell <jakesjews@gmail.com>
2011-11-26 21:59:26 -0500

    updated seeds to create only nonexisting records

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 nouns = ['action-items','applications','architectures','bandwidth','channels','communities','content','convergence','deliverables','e-business','e-commerce','e-markets','e-services','e-tailers','experiences','functionalities','infomediaries','infrastructures','initiatives','interfaces','markets','methodologies','metrics','mindshare','models','networks','niches','paradigms','partnerships','platforms','portals','relationships','ROI','synergies','web-readiness','schemas','solutions','supply-chains','systems','technologies','users','decision-trees','interoperability','XML Interfaces','JSON Interfaces','RSS Feeds''blogs','wikis','mashups','folksonomies','frameworks','blogospheres'].each do |noun|
-  Word.create([{ word_type: 'noun', word: noun }])
+  Word.find_or_create_by_word_type_and_word( :word_type => 'noun', :word => noun )
 end
 
 verbs = ['aggregate','architect','benchmark','brand','cultivate','deliver','deploy','disintermediate','drive','e-enable','embrace','empower','enable','engage','engineer','enhance','envisioneer','evolve','expedite','exploit','extend','facilitate','generate','grow','harness','implement','incentivize','incubate','innovate','integrate','iterate','leverage','maximize','mesh','monetize','morph','optimize','orchestrate','recontextualize','reintermediate','reinvent','repurpose','revolutionize','scale','seize','strategize','streamline','syndicate','synergize','synthesize','target','transform','transition','unleash','utilize','visualize','whiteboard'].each do |verb|
-  Word.create([{ word_type: 'verb', word: verb }])
+  Word.find_or_create_by_word_type_and_word( :word_type => 'verb', :word => verb )
 end
 
 adjectives = ['24/365','24/7','B2B','B2C','back-end','best-of-breed','bleeding-edge','bricks-and-clicks','clicks-and-mortar','collaborative','compelling','cross-platform','cross-media','customized','cutting-edge','distributed','dot-com','dynamic','e-business','efficient','end-to-end','enterprise','extensible','frictionless','front-end','global','granular','holistic','impactful','innovative','integrated','interactive','intuitive','killer','leading-edge','magnetic','mission-critical','next-generation','one-to-one','open-source','out-of-the-box','plug-and-play','proactive','real-time','revolutionary','robust','scalable','seamless','sticky','strategic','synergistic','transparent','turn-key','ubiquitous','user-centric','value-added','vertical','viral','virtual','visionary','web-enabled','wireless','world-class','cloud','social'].each do |adjec|
-  Word.create([{ word_type: 'adjective', word: adjec }])
+  Word.find_or_create_by_word_type_and_word( :word_type => 'adjective', :word => adjec )
 end
